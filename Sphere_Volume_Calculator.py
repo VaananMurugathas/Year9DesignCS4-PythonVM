21c511597512664791e9a21294a967c2149d9810
Vaanan Murugathas <vaanan.murugathas@10353.local>
2018-09-28 14:33:46 -0400

    Updated sphere calculator

--- Sphere_Volume_Calculator.py
+++ Sphere_Volume_Calculator.py
@@ -2,11 +2,14 @@
 import tkinter as tk
 import math
 
-def calculate():
+def submit():
 
+	print("Submit pressed")
 	r = float(entr.get())
-
-
+	v = math.pi*r*r*r*4/3
+	v = round(v,3)
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
 
 root = tk.Tk()
 root.title("Volume of a cylinder")
@@ -17,6 +20,11 @@ labr.pack()
 entr = tk.Entry(root)
 entr.pack()
 
+btn = tk.Button(root, text="Submit", command=submit)
+btn.pack()
+
 output = tk.Text(root, width=50, height=10, borderwidth=2, relief=tk.GROOVE)
+output.config(state="disabled")
+output.pack()
 
 root.mainloop()
